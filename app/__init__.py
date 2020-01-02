3d08560e77f8c48dbe9aa9f9d6aec16f77275768
kebasoh <ongatikebasoba@gmail.com>
2020-01-02 10:19:36 +0300

    edeted the README.md

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ bootstrap = Bootstrap()
 def create_app(config_name):
 	app = Flask(__name__)
 
-	#create the app configurations
+	# create the app configurations
 	app.config.from_object(config_options[config_name])
 
 	# Initializing flask extensions
